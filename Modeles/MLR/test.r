01a15fcafd27c573fd890b88e92958d8fc57f822
Nina <ndavalentin05@gmail.com>
2026-03-24 23:20:00 +0100

    correction bug

--- Modeles/MLR/test.r
+++ Modeles/MLR/test.r
@@ -1,3 +1,36 @@
+library(nnet)
+set.seed(123)
+
+# Changer de répartoire de travail : Session --> Set Working Directory --> To source file location
+
+df <- read.csv("../../data/public_data_waste_fee.csv")
+dechets_colonnes <- c("organic", "paper", "glass", "wood", "metal", "plastic", "raee", "texile", "other")
+
+df[dechets_colonnes][is.na(df[dechets_colonnes])] <- 0
+
+index <- sample(1:nrow(df), size = 0.8 * nrow(df))
+train_set <- df[index, ]
+test_set  <- df[-index, ]
+
+Y_train <- as.matrix(train_set[, dechets_colonnes])
+
+modele_dechets <- multinom(Y_train ~ gdp + pden + alt + pop + urb, 
+                           data = train_set, 
+                           maxit = 500)
+
+summary(modele_dechets)
+
+#Validation croisée
+k <- 5  
+
+df$vrai_label <- dechets_colonnes[max.col(df[, dechets_colonnes])]
+df_equilibre <- df[df$vrai_label != "organic", ] 
+df_organic_reduit <- df[df$vrai_label == "organic", ][1:200, ] # On limite l'organique à 200 lignes
+df_final <- rbind(df_equilibre, df_organic_reduit)
+
+folds <- cut(seq(1, nrow(df_final)), breaks = k, labels = FALSE)
+folds <- sample(folds)
+
 accuracies <- c()
 all_preds <- c()
 all_vrais_labels <- c()
