c158ec743ef78b8e7c369b7b7c1c11f9391f5b99
Anthony253400 <mirandanthony2005@yahoo.fr>
2026-03-24 22:50:31 +0100

    ajout régression linéaire suppr

--- Modeles/RL/Régression linéaire suppression.py
+++ Modeles/RL/Régression linéaire suppression.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.impute import SimpleImputer
 from sklearn.metrics import r2_score, mean_squared_error
 
 df = pd.read_csv("data/public_data_waste_fee.csv")
@@ -10,24 +9,24 @@ df = pd.read_csv("data/public_data_waste_fee.csv")
 features = ['pop', 'msw', 'sor', 'tc', 'wage', 'area', 'isle', 'sea', 'urb', 'gdp', 'alt', 'roads', 'pden']
 waste_types = ["organic", "paper", "glass", "wood", "metal", "plastic", "raee", "textile", "other"]
 
-imputer = SimpleImputer(strategy='mean')
-
 print(f"{'DÉCHET':<12} | {'Lignes':<6} | {'R²':<6} | {'RMSE (%)':<10}")
 print("-" * 45)
+delta = []
 
 for waste in waste_types:
     if waste not in df.columns:
         continue
+    colone = features + [waste]
+    temp_df = df[colone].dropna()
 
-    mask = df[waste].notna()
-    X_current = df.loc[mask, features]
-    y_current = df.loc[mask, waste]
+    delta.append(len(df) -len(temp_df) )
 
-    if len(y_current) < 10:
-        continue
 
-    X_imputed = imputer.fit_transform(X_current)
-    X_train, X_test, y_train, y_test = train_test_split(X_imputed, y_current, test_size=0.2, random_state=42)
+
+    X_current = temp_df[features]
+    y_current = temp_df[waste]
+
+    X_train, X_test, y_train, y_test = train_test_split(X_current, y_current, test_size=0.2, random_state=42)
 
     model = LinearRegression()
     model.fit(X_train, y_train)
@@ -39,4 +38,7 @@ for waste in waste_types:
     mean_val = y_test.mean()
     rmse_pct = (rmse / mean_val) * 100 if mean_val != 0 else 0
 
-    print(f"[{waste:<10}] | Lignes: {len(y_current):<4} | R²: {r2:<5.2f} | RMSE: ±{rmse_pct:.2f}%")
+    print(f"[{waste:<10}] | Lignes: {len(temp_df):<4} | R²: {r2:<5.2f} | RMSE: ±{rmse_pct:.2f}%")
+
+
+print(delta)
