cce2cdbb5876c9091b9aa35c6fa5843e46ed4649
HappyTrip <faizambre@gmail.com>
2026-03-29 17:18:08 +0200

    modif rl

--- Modeles/RegressionLineaire/Régression linéaire moyenne.py
+++ Modeles/RegressionLineaire/Régression linéaire moyenne.py
@@ -6,14 +6,15 @@ from sklearn.impute import SimpleImputer
 from sklearn.metrics import r2_score, mean_squared_error
 
 df = pd.read_csv("data/public_data_waste_fee.csv")
+df.rename(columns={'texile': 'textile'}, inplace=True)
 
-features = ['pop', 'msw', 'sor', 'tc', 'wage', 'area', 'isle', 'sea', 'urb', 'gdp', 'roads','alt', 'pden']
-waste_types = ["organic", "paper", "glass", "wood", "metal", "plastic", "raee", "texile", "other"]
+features = ['pop', 'tc', 'wage', 'area', 'urb', 'gdp', 'roads','alt', 'pden', 'd_fee']
+waste_types = ["organic", "paper", "glass", "wood", "metal", "plastic", "raee", "textile", "other"]
 
 imputer = SimpleImputer(strategy='mean')
 
-print(f"{'DÉCHET':<12} | {'Lignes':<6} | {'R²':<6} | {'RMSE (%)':<10}")
-print("-" * 45)
+print(f"{'DÉCHET':<12} | {'R²':<5} | {'RMSE ABS':<10} | {'MOYENNE':<10} | {'ERREUR RELATIVE (NRMSE)':<20}")
+print("-" * 85)
 
 for waste in waste_types:
     if waste not in df.columns:
@@ -23,20 +24,22 @@ for waste in waste_types:
     X_current = df.loc[mask, features]
     y_current = df.loc[mask, waste]
 
-    if len(y_current) < 10:
+    if len(y_current) < 25:
         continue
 
     X_imputed = imputer.fit_transform(X_current)
+    
     X_train, X_test, y_train, y_test = train_test_split(X_imputed, y_current, test_size=0.2, random_state=42)
 
     model = LinearRegression()
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
-    r2 = r2_score(y_test, y_pred)
-    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
     
-    mean_val = y_test.mean()
-    rmse_pct = (rmse / mean_val) * 100 if mean_val != 0 else 0
+    r2 = r2_score(y_test, y_pred)
+    rmse_abs = np.sqrt(mean_squared_error(y_test, y_pred))
+    moyenne_reelle = y_test.mean()
+    
+    nrmse = (rmse_abs / moyenne_reelle * 100) if moyenne_reelle != 0 else 0
 
-    print(f"[{waste:<10}] | Lignes: {len(y_current):<4} | R²: {r2:<5.2f} | RMSE: ±{rmse_pct:.2f}%")
+    print(f"{waste:<12} | {r2:5.2f} | ±{rmse_abs:7.2f}% | {moyenne_reelle:9.2f}% | {nrmse:6.1f}% de la moyenne")
