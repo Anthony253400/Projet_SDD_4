e9b0e8de5e9dd3733609853e247aae1bae4fc2db
Nina <ndavalentin05@gmail.com>
2026-04-05 15:30:51 +0200

    liens logos

--- ApplicationWeb/graphiques.php
+++ ApplicationWeb/graphiques.php
@@ -6,6 +6,7 @@
     <title>Analyses Graphiques | L3 MIASHS</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
         /* On garde exactement ton style de base */
         body { background-color: #f8f9fa; }
