6c3aefe987f9ef2c7b7dc42d64f442c83c045510
Anthony253400 <mirandanthony2005@yahoo.fr>
2026-04-06 19:21:39 +0200

    netoyage

--- Modeles/CNN/teste.py
+++
@@ -1,6 +0,0 @@
-import os
-classes = ["cardboard", "glass", "metal", "paper", "plastic", "trash"]
-
-for c in classes:
-    n = len(os.listdir(f"data/data_split/test/{c}"))  # adapte le chemin
-    print(f"{c} : {n} images")
